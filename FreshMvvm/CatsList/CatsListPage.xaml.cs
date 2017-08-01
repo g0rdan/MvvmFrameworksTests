eeadbf265f1b217e9094855c78df9053f91518df
Denis Gordin <gordin.dan@gmail.com>
2017-08-01 14:49:40 +0600

    add empty CatsList project to FreshMvvm folder

---
+++ FreshMvvm/CatsList/CatsListPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CatsList
+{
+    public partial class CatsListPage : ContentPage
+    {
+        public CatsListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
